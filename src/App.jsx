71114d5d2e8343f7bcdddbad87081185cb4b1ec4
RinSanom <rinsanom3@gmail.com>
2024-09-16 08:57:18 -0700

    Clear

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,5 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-900">
-        Hello world!
-      </h1>
-      <h1 className="text-3xl font-bold underline text-red-900">
-        Hello world!
-      </h1>
-      <h1 className="text-3xl font-bold underline text-red-900">
-        Hello world!
-      </h1>
-      <h1 className="text-3xl font-bold underline text-red-900">
-        Hello world!
-      </h1>
-      <h1 className="text-3xl font-bold underline text-red-900">
-        Hello world!
-      </h1>
-    </>
-  );
 }
 export default App;

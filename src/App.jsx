17341c09d198153ddf50d49c69a2f06946447627
kimchamraoen <kimchamraoen@gmail.com>
2024-09-17 09:27:53 +0700

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  <>
-    <h1>Hello !!!! </h1>
+  
+  return<>
+    <h1 className="text-blue-600">Hello !!!! </h1>
   </>;
 }
 export default App;

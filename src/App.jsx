afd5a33d1784e7c63f4df36ef48006732bcd0bda
kimchamraoen <kimchamraoen@gmail.com>
2024-09-19 10:26:22 +0700

    forget password

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
+// import { ForgotPassword} from "./pages/Auth/ForgotPassword"
+import { NewPassword } from "./pages/Auth/NewPassword";
 
 function App() {
   
   return<>
-    <h1 className="text-blue-600">Hello !!!! </h1>
+    {/* <ForgotPassword/> */}
+    <NewPassword/>
   </>;
 }
 export default App;
+
---
+++ src/pages/Auth/ForgotPassword.jsx
@@ -0,0 +1,65 @@
+import { Button, Label, TextInput } from "flowbite-react";
+
+export function NewPassword() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle password submission logic here
+  };
+
+  return (
+    <div className="flex flex-col h-screen">
+      <h1 className="font-bold text-2xl text-center mt-[3rem] mb-10 sm:mt-16 sm:mb-8">
+        New Password
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 xl:mx-auto">
+        <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+          <div className="mb-5">
+            <label
+              htmlFor="new-password"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Enter New Password
+            </label>
+            <input
+              type="password"
+              id="new-password"
+              className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 sm:w-72 lg:w-96"
+              required
+              placeholder="8 characters at least"
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="confirm-password"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="confirm-password"
+              className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              required
+            />
+          </div>
+
+          {/* Centering the button */}
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="text-white bg-blue-900 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-56 px-5 py-2.5"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+
+        <img
+          src="https://img.freepik.com/premium-vector/forgot-password-account-login-web-page-protection-security-key-access-system-smartphone-computer-flat-vector-illustration_2175-1375.jpg?w=996"
+          alt="Forgot password illustration"
+          className="mx-auto max-w-full md:max-w-lg md:mt-0 mt-4"
+        />
+      </div>
+    </div>
+  );
+}

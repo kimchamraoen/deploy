8b21482f46015de4bdaed3d51b357b8b5d25baf4
kimchamraoen <kimchamraoen@gmail.com>
2024-09-20 00:35:51 +0700

    code

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,60 @@ import { NewPassword } from "./pages/Auth/NewPassword";
 import React from "react";
 import { NavbarComponent } from "./components/layout/Navbar";
 
-function App() {
-  
+const App = () =>{
+  const [products, setProducts] = useState([]); // products =[]
+  const [loading, setLoading] = useState(false);
+
+  console.log("products", products);
+  // get user profile
+  useEffect(() => {
+    async function fetchUserProfile() {
+      const accessToken = getAccessToken();
+      console.log("accessToken", accessToken);
+      const userData = await getUserProfile(accessToken);
+      console.log("userData", userData);
+    }
+
+    fetchUserProfile();
+  }, []);
+  useEffect(() => {
+    async function fetchAllProducts() {
+      setLoading(true);
+      let product = await getAllProducts();
+      setProducts(product);
+      setLoading(false);
+    }
+    fetchAllProducts();
+  }, []);
   return<>
     {/* <ForgotPassword/> */}
     {/* <NewPassword/> */}
+    <main className="max-w-screen-2xl mx-auto px-5">
+      <section>
+        <h1 className="text-blue-800 font-bold text-4xl text-center mt-5">
+          List Products
+        </h1>
+        <img src="javascript.png" alt="" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+          {loading && (
+            <img src="https://cdn.pixabay.com/animation/2023/10/08/03/19/03-19-26-213_512.gif" />
+          )}
+          {!loading &&
+            products
+              .slice(0, 8)
+              .map((product, index) => (
+                <ProductCard
+                  key={index}
+                  title={product.title}
+                  image={product.images[0]}
+                  price={product.price}
+                  brand={product.brand}
+                  id={product.id}
+                />
+              ))}
+        </div>
+      </section>
+    </main>
   </>;
 }
 export default App;
-
-// =======
-// import React from "react";
-// import { NavbarComponent } from "./components/layout/Navbar";
-// export default function App() {}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import RootLayout from "./components/layout/RootLayout.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./components/layouts/RootLayout.jsx";
+import Register from "./pages/auth/Register.jsx";
+import Product from "./pages/product/Product.jsx";
+import ProductDetail from "./pages/productDetail/ProductDetail.jsx";
 import Login from "./pages/auth/Login.jsx";
-import Resgister from "./pages/auth/Register.jsx";
-import ForgotPassword from "./pages/auth/ForgotPassword.jsx";
-import Verifyotp from "./pages/auth/Verifyotp.jsx";
+import Verify from "./pages/auth/Verify.jsx";
+import Job from "./pages/job/Job.jsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -15,29 +18,38 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <App />,
+        element: <App />
       },
-    ],
+      // {
+      //   path: "/products",
+      //   element: <Product />
+      // },
+      // {
+      //   path: "/products/:id",
+      //   element: <ProductDetail />
+      // },
+      // {
+      //   path: "/jobs",
+      //   element: <Job />
+      // }
+    ]
   },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Resgister />,
-  },
-  {
-    path: "/ForgotPassword",
-    element: <ForgotPassword />,
-  },
-  {
-    path: "/verifyotp",
-    element: <Verifyotp />,
-  },
+  // {
+  //   path: "/register",
+  //   element: <Register />
+  // },
+  // {
+  //   path: "/login",
+  //   element: <Login />
+  // },
+  // {
+  //   path: "/verify-email",
+  //   element: <Verify />
+  // }
 ]);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={router} />
   </StrictMode>
 );

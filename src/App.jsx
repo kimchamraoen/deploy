3b56efe856b1a274ba5760518e1b61cb34a42b39
RinSanom <rinsanom3@gmail.com>
2024-09-21 22:56:52 -0700

    Navy commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,6 @@ function App() {
 
   return (
     <>
-      {/* <NavbarComponent /> */}
-      {/* <NewPassword/> */}
-      {/* <ProfileCard/> */}
       <div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
           {products.map((product) => (
